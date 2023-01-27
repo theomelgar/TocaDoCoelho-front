db80a535e8708afabf9ceb86fc2a4b28d31320a6
Andre <andre.eng1313@gmail.com>
2023-01-27 19:44:21 -0300

    pagina carrinho com os dados

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -1,44 +1,42 @@
-import axios from "axios"
 import Footer from "../components/Footer.js";
 import Header from "../components/Header.js";
 import styled from "styled-components";
-import { useState } from "react"
-import { useEffect } from "react"
+import { useContext, useState } from "react"
+import { ItensContext } from "../context/itens.js";
 
 
 export default function Carrinho() {
 
+    const { cesta, total } = useContext(ItensContext)
+  
 
-    const { quantidade, setQuantidade } = useState()
-    const { produto, setProduto } = useState([])
-
-
-
-    useEffect(() => {
-        axios.get("http://localhost:5005/carrinho")
-            .then(res => {
-                setProduto(res.data)
-                setQuantidade()
-
-            })
-            .catch(err => {
-                alert(err.data)
-            })
-
-    }, [])
-
-
- function comprar () {
-
- }
-
-
+    console.log(cesta)
+    
     return (
+
         <StyleProdutos>
+
             <Header />
 
+            <h1> Carrinho </h1>
 
-            <ComprarBtn onClick={comprar}>COMPRAR</ComprarBtn>
+            <Compras>
+
+
+            {cesta.map((c) => (
+                
+                
+                <Compra> <h2> {c.produto} - Qtd: {c.quantidade} - R$ {c.total}</h2> </Compra>)
+
+                )}
+
+                <ComprarBtn>COMPRAR</ComprarBtn>
+
+                <Botao>Continuar comprando</Botao>
+
+            </Compras>
+
+            <h2> Total: R$ {total} </h2>
 
 
             <Footer />
@@ -48,9 +46,10 @@ export default function Carrinho() {
 }
 
 const StyleProdutos = styled.div`
-    position: relative;
+   
     width: 100vw;
     height: 100vh;
+    margin: 150px 0; 
 `
 
 
@@ -61,7 +60,8 @@ const ComprarBtn = styled.div`
         transform: scale(1.1);
     }
     position: absolute;
-    bottom: -35%;
+    left: 40%;
+    bottom: -135%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -76,3 +76,43 @@ const ComprarBtn = styled.div`
     font-weight: 700;
     font-size: 20px;
     line-height: 23px;`
+
+
+
+const Compras = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: blue;
+    gap: 10px;
+    position: relative;
+    `
+
+const Compra = styled.div`
+    font-family: Rubik;
+    font-size: 25px;
+    color: green;
+    background: red;
+    `
+
+const Botao = styled.div`
+cursor: pointer;
+:hover{
+    opacity: 0.8;
+    transform: scale(1.1);
+}
+position: absolute;
+left: 40%;
+bottom: -240%;
+display: flex;
+justify-content: center;
+align-items: center;
+color: #274D00;
+width: 242px;
+height: 41px;
+border-radius: 10px;
+border: none;
+font-family: 'Raleway';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 23px;`
